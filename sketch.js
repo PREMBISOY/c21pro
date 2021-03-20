cf219036ed6958b730a941c596e257959b77557c
PREMBISOY <77225599+PREMBISOY@users.noreply.github.com>
2021-03-20 22:14:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var wall,thickness;
+var speed,bullet;
+//var bulletRightEdge,wallLeftEdge;
+
+
+
+
+
+function setup() {
+  createCanvas(1600,400);
+  thickness=random(22,83);
+weight = random(30,50);
+speed = random(10,15)
+bullet = createSprite(100,200,50,20);
+
+bullet.velocityX = speed;
+wall = createSprite(1200,200,thickness,height/2);
+wall.shapecolor=color(80,80,80);
+
+
+}
+
+function draw() {
+  background("black");
+  if(wall.x-bullet.x < (bullet.width+wall.width/2)){
+bullet.velocityX=0;
+var damage=0.5 * weight * speed * speed/thickness*thickness*thickness;
+
+if(damage<10){
+wall.shapeColor = color(0,255,0);
+}
+
+if(damage>10){
+  wall.shapeColor = color(255,0,0);
+
+  }
+
+
+  }
+  drawSprites();
+}
+
+
+/*function hasCollided(lbullet,lwall){
+bulletRightEdge=lbullet.x + lbullet.width;
+wallLeftEdge=lwall.x;
+if(bulletRightEdge>=wallLeftEdge)
+{
+return true;
+
+}
+return false;
+}*/
